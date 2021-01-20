4e76c95c405e51286dfb830fbf068e3f6ddf4cec
Stephano Marques <59531590+stephanomarques@users.noreply.github.com>
2021-01-20 03:52:54 +0000

    -> Home Fragment FINISHED

--- app/src/main/java/com/example/newsapplication/api/EndPoints.kt
+++ app/src/main/java/com/example/newsapplication/api/EndPoints.kt
@@ -32,4 +32,8 @@ interface EndPoints {
     @GET("top-headlines?lang=en&token=33373c71ba6ddbba81aaf657a8772c3d&max=10&topic=science")
     fun getScienceNews(): Call<ResponseModel>
 
+    //Get Home Headlines
+    @GET("top-headlines?lang=en&token=33373c71ba6ddbba81aaf657a8772c3d&max=10&topic=world")
+    fun getHomeNews(): Call<ResponseModel>
+
 }
--- app/src/main/java/com/example/newsapplication/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/newsapplication/ui/home/HomeFragment.kt
@@ -5,27 +5,66 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.newsapplication.R
+import com.example.newsapplication.adapter.NewsAdapter
+import com.example.newsapplication.api.EndPoints
+import com.example.newsapplication.api.ServiceBuilder
+import com.example.newsapplication.entities.News
+import com.example.newsapplication.entities.ResponseModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private var thisLayoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<NewsAdapter.NewsHolder>? = null
+    private lateinit var sampleNews: List<News>
+    private var mAdapter: NewsAdapter? = null
 
+    //On View Creation////////////////////////////////////////////////////////////////////////////
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }////////////////////////////////////////////////////////////////////////////////////////////
+
+    //Upon Creating View////////////////////////////////////////////////////////////////////////////
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //Initialize EndPoints and Create List to contain all markers
+        val request = ServiceBuilder.buildService(EndPoints::class.java)
+
+        //recycler
+        val recyclerView: RecyclerView = view.findViewById(R.id.home_recyclerview)
+        val layoutManager: RecyclerView.LayoutManager = LinearLayoutManager(context)
+        recyclerView.layoutManager = layoutManager
+
+        //Get health news request and pass to adapter////////////////////////////////////////////
+        val call: Call<ResponseModel> = request.getHomeNews()
+        call.enqueue(object : Callback<ResponseModel> {
+            override fun onResponse(call: Call<ResponseModel>, response: Response<ResponseModel>) {
+                if (response.isSuccessful) {
+                    val theseNews: List<News>? = response.body()!!.allNews
+                    if(theseNews != null){
+                        mAdapter = context?.let { NewsAdapter(it, theseNews) }
+                        recyclerView.adapter = mAdapter
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseModel>, t: Throwable) {
+                Toast.makeText(activity, "${t.message}", Toast.LENGTH_SHORT).show()
+            }
         })
-        return root
-    }
+    }////////////////////////////////////////////////////////////////////////////////////////////
 }
